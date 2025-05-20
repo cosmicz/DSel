a97879fd1f589e74a7bc45c8149ba0f1116606ca
Octavian <cosmicz@protonmail.com>
2025-05-19 17:42:13 -0700

    refine: use dsel--defparameter for var defs, clean examples comments

--- dsel-macros.el
+++ dsel-macros.el
@@ -59,6 +59,16 @@
 
 ;;; Core definition macros
 
+(defmacro dsel--defparameter (var value &optional docstring)
+  "Define a parameter VAR with VALUE and optional DOCSTRING.
+Update the parameter if it is already defined."
+  (declare (indent 1))
+  `(progn
+     (if (boundp ',var)
+         (setq ,var ,value)
+       (defvar ,var ,value ,docstring))
+     ',var))
+
 (defmacro dsel-defsignature (name instructions &rest plist)
   "Define a DSel signature named NAME with INSTRUCTIONS and PLIST properties.
 The signature is stored in the variable NAME.
@@ -66,7 +76,7 @@ PLIST is a keyword argument list for `dsel-make-signature'
 (e.g., :input-fields ..., :output-fields ..., :name can be overridden).
 If :name is not in PLIST, NAME is used as the signature's internal name."
   (declare (indent 2)) ; For proper indentation support
-  `(defvar ,name
+  `(dsel--defparameter ,name
      (dsel-make-signature
       ,instructions
       ,@(if (plist-member plist :name)
@@ -81,7 +91,7 @@ PLIST is a keyword argument list for `dsel-make-predict'
 (e.g., :config ..., :demos ..., :lm ..., :name can be overridden).
 If :name is not in PLIST, NAME is used as the predictor's internal name."
   (declare (indent 2))
-  `(defvar ,name
+  `(dsel--defparameter ,name
      (dsel-make-predict
       ,signature-var ; Assumes signature-var holds an already defined dsel-signature
       ,@(if (plist-member plist :name)
@@ -98,40 +108,13 @@ INPUT-KEYS-FORM should evaluate to a list of symbols, e.g., '(key1 key2) or just
   (declare (indent 2))
   (let ((docstring (format "A list of dsel-examples for %s with input keys derived from %s."
                            var-name input-keys-form)))
-    `(defvar ,var-name
+    `(dsel--defparameter ,var-name
        (dsel-create-examples
         ;; List of plists for each example definition
         ',(mapcar (lambda (def-plist) def-plist) example-definitions-plists)
         ;; Keyword argument for input-keys
         :input-keys ,input-keys-form)
        ,docstring)))
-
-;; ;;Usage:
-;; (dsel-defexamples my-examples '(a) ; input-keys-form evaluates to ('a)
-;;                   (:a "one" :b "1")
-;;                   (:a "two" :b "2"))
-
-;; (dsel-defexamples another-set '(text question)
-;;                   (:text "ctx1" :question "q1" :answer "a1")
-;;                   (:text "ctx2" :question "q2" :answer "a2"))
-
-;; (let ((keys '(input1)))
-;;   (dsel-defexamples dynamic-examples keys
-;;                     (:input1 "val1" :output1 "res1")))
-
-;; ;; Example usage for dsel-defexamples:
-;; (dsel-defexamples example-set-1 '(a) ; Input keys list
-;;                   (:a "one" :b "1")
-;;                   (:a "two" :b "2"))
-
-;; (dsel-defexamples example-set-2 '(text question) ; Multiple input keys
-;;                   (:text "context" :question "q1" :answer "ans1")
-;;                   (:text "context2" :question "q2" :answer "ans2"))
-
-;; (let ((common-inputs '(a)))
-;;   (dsel-defexamples example-set-3 common-inputs ; Using a variable for input keys
-;;                     (:a "one" :b "1")
-;;                     (:a "two" :b "2")))
 
 (defmacro dsel-defchain-of-thought (name original-signature &rest plist)
   "Define a DSel chain-of-thought module named NAME using ORIGINAL-SIGNATURE and PLIST.
@@ -140,7 +123,7 @@ PLIST is a keyword argument list for `dsel-make-chain-of-thought'
 (e.g., :rationale-field-name ..., :config ..., :name can be overridden).
 If :name is not in PLIST, NAME is used as the module's internal name."
   (declare (indent 2))
-  `(defvar ,name
+  `(dsel--defparameter ,name
      (dsel-make-chain-of-thought
       ,original-signature
       ,@(if (plist-member plist :name)
@@ -157,7 +140,7 @@ PLIST is a keyword argument list for the corresponding make function
 If :name is in PLIST, it will be ignored as optimizers don't have internal names."
   (declare (indent 2))
   (let ((maker-function (intern (format "dsel-make-%s" (symbol-name type)))))
-    `(defvar ,name
+    `(dsel--defparameter ,name
        (,(if (fboundp maker-function)
              maker-function
            (error "No maker function for optimizer type %s" type))
